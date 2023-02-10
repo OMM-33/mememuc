8fc9043d9b7c503ed818f86d9e14ee22eb3b0bde
ines29 <i.trautmannsheimer@campus.lmu.de>
2023-02-10 11:48:36 +0100

    First Overview Grid, without function

---
+++ client/src/lib/Overview/Frame.svelte
@@ -0,0 +1,44 @@
+<script>
+
+
+
+	export let upvotes = 12;
+	export let views = 3;
+	export let comments = 2;
+	export let image;
+</script>
+<div class="container">
+	<div style:background-image="url('{image}')" class="image" />
+	<div class="info"><span>⬆️{upvotes} </span><span class="middle">👁️{views}</span><span class="right">🗨️ {comments}</span>
+	</div>
+</div>
+
+<style>
+        .container{
+            width: 100%;
+            margin-top:0px;
+            margin-bottom: 20%;
+
+        }
+        span{
+            padding-left: 10%;
+            padding-right: 10%;
+            padding-top: 3%;
+        }
+        .info{
+            display: flex;
+            justify-content: center;
+            width: 100%;
+        }
+        .image{
+            width:20vw;
+            height: 32vh;
+            background-size: contain;
+            background-repeat: no-repeat;
+            background-position: center;
+            background-color: #F2F9F2;
+            border: 1px solid black;
+
+        }
+
+</style>
--- client/src/views/Overview.svelte
+++ client/src/views/Overview.svelte
@@ -1,1 +1,38 @@
-<h1>Overview</h1>
+<h1>Overview </h1>
+<script>
+	import Frame from "../lib/Overview/Frame.svelte";
+
+	const images =[{ stats: { upvotes: 2,views:5,comments:1 },
+		src: "https://i.imgur.com/T8Hhkur.jpeg",
+	},{ stats: { upvotes: 2,views:5,comments:1 },
+		src: "https://i.imgur.com/Uschheg.jpeg",
+	},{ stats: { upvotes: 2,views:5,comments:1 },
+		src: "https://i.imgur.com/Jvh1OQm.jpeg",
+	},{ stats: { upvotes: 2,views:5,comments:1 },
+		src: "https://i.imgur.com/jlFgGpe.jpeg",
+	},{ stats: { upvotes: 2,views:5,comments:1 },
+		src: "https://i.imgur.com/K6U0L3q.jpeg",
+	},{ stats: { upvotes: 2,views:5,comments:1 },
+		src: "https://i.imgur.com/rFBiyeR.jpeg",
+	},{ stats: { upvotes: 2,views:5,comments:1 },
+		src: "https://i.imgur.com/g81WFjQ.jpeg",
+	},{ stats: { upvotes: 2,views:5,comments:1 },
+		src: "https://i.imgur.com/0LINzxs.jpeg",
+	},{ stats: { upvotes: 2,views:5,comments:1 },
+		src: "https://i.imgur.com/xp4WKjC.jpeg",
+	}];
+
+</script>
+<div class="grid">
+	{#each images as image}
+		<Frame image={image.src} upvotes={image.stats.upvotes} views={image.stats.views} comments={image.stats.comments} />
+	{/each}
+</div>
+
+<style>
+	.grid{
+		display: grid;
+		grid-template-columns:auto auto auto;
+		justify-content: space-between;
+	}
+</style>
