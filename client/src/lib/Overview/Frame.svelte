a66c57045bf594bc3f2dc9cde5876d128539b23f
ines29 <58683420+ines29@users.noreply.github.com>
2023-02-15 18:11:36 +0100

    First Overview Grid, without function (#29)
    * First Overview Grid, without function
    
    * requested changes
    
    * added title

---
+++ client/src/lib/Overview/Frame.svelte
@@ -0,0 +1,48 @@
+<script>
+	export let upvotes = 12;
+	export let views = 3;
+	export let comments = 2;
+	export let image;
+	export let title;
+</script>
+<div class="container">
+	<p id="title"> {title}</p>
+	<div style:background-image="url('{image}')" class="image" />
+	<div class="info"><span>⬆️{upvotes} </span><span class="middle">👁️{views}</span><span class="right">🗨️ {comments}</span>
+	</div>
+</div>
+
+<style>
+	.container{
+		width: 100%;
+		margin-top:0px;
+		margin-bottom: 20%;
+	}
+	span{
+		padding-left: 10%;
+		padding-right: 10%;
+		padding-top: 3%;
+		}
+	.info{
+		display: flex;
+		justify-content: center;
+		width: 100%;
+		}
+	.image{
+		width:100%;
+		height: 32vh;
+		background-size: contain;
+		background-repeat: no-repeat;
+		background-position: center;
+		background-color: #F2F9F2;
+		border: 1px solid black;
+
+		}
+	#title{
+		text-align: center;
+		font-size: 1.2em;
+		font-weight: bold;
+
+	}
+
+</style>
--- client/src/views/Overview.svelte
+++ client/src/views/Overview.svelte
@@ -1,1 +1,38 @@
-<h1>Overview</h1>
+<h1>Overview </h1>
+<script>
+	import Frame from "../lib/Overview/Frame.svelte";
+
+	const images = [{titel:"Titel", stats: { upvotes: 2,views:5,comments:1 },
+		src: "https://i.imgur.com/T8Hhkur.jpeg",
+	},{ titel:"Titel",stats: { upvotes: 2,views:5,comments:1 },
+		src: "https://i.imgur.com/Uschheg.jpeg",
+	},{ titel:"Sehr sehr sehr sehr  langer Titel",stats: { upvotes: 2,views:5,comments:1 },
+		src: "https://i.imgur.com/Jvh1OQm.jpeg",
+	},{ titel:"Titel",stats: { upvotes: 2,views:5,comments:1 },
+		src: "https://i.imgur.com/jlFgGpe.jpeg",
+	},{ titel:"Titel",stats: { upvotes: 2,views:5,comments:1 },
+		src: "https://i.imgur.com/K6U0L3q.jpeg",
+	},{ titel:"Titel",stats: { upvotes: 2,views:5,comments:1 },
+		src: "https://i.imgur.com/rFBiyeR.jpeg",
+	},{ titel:"Langer Titel",stats: { upvotes: 2,views:5,comments:1 },
+		src: "https://i.imgur.com/g81WFjQ.jpeg",
+	},{ titel:"Titel",stats: { upvotes: 2,views:5,comments:1 },
+		src: "https://i.imgur.com/0LINzxs.jpeg",
+	},{ titel:"Titel",stats: { upvotes: 2,views:5,comments:1 },
+		src: "https://i.imgur.com/xp4WKjC.jpeg",
+	}];
+
+</script>
+<div class="grid">
+	{#each images as image}
+		<Frame title={image.titel} image={image.src} upvotes={image.stats.upvotes} views={image.stats.views} comments={image.stats.comments} />
+	{/each}
+</div>
+
+<style>
+	.grid{
+		display: grid;
+		grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
+		justify-content: space-between;
+	}
+</style>
