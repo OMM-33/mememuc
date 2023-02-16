b26ce17ee6246551a8ea77cb12bab0faba70e20c
ines29 <i.trautmannsheimer@campus.lmu.de>
2023-02-16 16:40:27 +0100

    Neue Funktionalitäten
    -Detail View
    -Kommentare
    -Upvotes
    -Views

---
+++ client/src/lib/View/Comment.svelte
@@ -0,0 +1,23 @@
+<script>
+	export let name;
+	export let text;
+</script>
+
+<div id="background">
+	<p id="name">{name}</p>
+	<p id="text">{text}</p>
+</div>
+
+<style>
+	#background{
+		background-color: white;
+	}
+	p{
+		color: black;
+		padding-left: 3%;
+	}
+	#name{
+		font-weight: bold;
+		background-color: #26ba89;
+	}
+</style>
