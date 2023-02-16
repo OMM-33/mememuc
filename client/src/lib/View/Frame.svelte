93cce295dab37c487d31bdb7e676c433932b3390
ines29 <i.trautmannsheimer@campus.lmu.de>
2023-02-16 16:40:27 +0100

    Neue Funktionalitäten
    -Detail View
    -Kommentare
    -Upvotes
    -Views

---
+++ client/src/lib/View/Frame.svelte
@@ -0,0 +1,76 @@
+<script>
+	export let upvotes = 12;
+	export let views = 3;
+	export let comments = 2;
+	export let image;
+	export let title;
+	export let upvoted;
+	export let clicked;
+
+	function handleUpvote(){
+		upvotes++;
+		upvoted = true;
+	}
+	function details(){
+		views++;
+		console.log('Viewed: '+views);
+		clicked = true;}
+</script>
+<div class="container">
+	<p id="title">{title}</p>
+	<button style:background-image="url('{image}')" id="image" on:click={details} />
+	<div class="info"><button id="upvoteButton" on:click|once={handleUpvote} class:upvoted={upvoted === true}> ⬆️ {upvotes}</button><span class="views">👁️ {views}</span><span class="comments">🗨️ {comments}</span>
+	</div>
+</div>
+
+<style>
+	.container{
+		width: 100%;
+		margin-top:0px;
+		margin-bottom: 20%;
+	}
+	span{
+		padding-left: 10%;
+		padding-right: 10%;
+		padding-top: 3%;
+	}
+	.info{
+		display: flex;
+		justify-content: center;
+		width: 100%;
+	}
+	#image{
+		width:100%;
+		height: 32vh;
+		background-size: contain;
+		background-repeat: no-repeat;
+		background-position: center;
+		background-color: #F2F9F2;
+		border: 1px solid black;
+		cursor: pointer;
+	}
+	#title{
+		margin: 0 5% 5% 5%;
+		text-align: center;
+		font-size: 1.2em;
+		font-weight: bold;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		white-space: nowrap;
+
+	}
+	#upvoteButton{
+  background-color: none;
+  border: none;
+  margin-left: 10%;
+  margin-right: 10%;
+  margin-top: 3%;
+  text-align: center;
+  text-decoration: none;
+  cursor: pointer;
+ }
+ .upvoted{
+  cursor: pointer;
+  background-color:rgb(196, 192, 192);
+}
+</style>
--- client/src/views/Overview.svelte
+++ client/src/views/Overview.svelte
@@ -1,33 +1,38 @@
 <h1>Overview </h1>
 <script>
-	import Frame from "../lib/Overview/Frame.svelte";
+	import Frame from "../lib/View/frame.svelte";
+	import Overlay from "../lib/Overlay.svelte";
+	import DetailView from "../lib/View/DetailView.svelte";
 
-	const images = [{titel:"Titel", stats: { upvotes: 2,views:5,comments:1 },
+	const images = [{ id:1,title:"Titel", stats: { upvotes: 2,upvoted: false,views:7,comments:1 ,clicked: false }, comments:[{ name:"Ines",text:"I love cats" },{ name:"Felix",text:"I don't like cats" }],
 		src: "https://i.imgur.com/T8Hhkur.jpeg",
-	},{ titel:"Titel",stats: { upvotes: 2,views:5,comments:1 },
+	},{ id: 2,title:"Titel",stats: { upvotes: 2,upvoted: true,views:5,comments:1 }, comments:[{ name:"Ines",text:"I love cats" },{ name:"Felix",text:"I don't like cats" }],
 		src: "https://i.imgur.com/Uschheg.jpeg",
-	},{ titel:"Sehr sehr sehr sehr  langer Titel",stats: { upvotes: 2,views:5,comments:1 },
+	},{ id:3, title:"Sehr sehr sehr sehr sehr sehr sehr sehr langer Titel",stats: { upvotes: 2,upvoted: false,views:5,comments:1,clicked: false },comments:[{ name:"Ines",text:"I love cats" },{ name:"Felix",text:"I don't like cats" }] ,
 		src: "https://i.imgur.com/Jvh1OQm.jpeg",
-	},{ titel:"Titel",stats: { upvotes: 2,views:5,comments:1 },
+	},{ id:4, title:"Titel",stats: { upvotes: 2,upvoted: false,views:5,comments:1,clicked: false },comments:[{ name:"Ines",text:"I love cats" },{ name:"Felix",text:"I don't like cats" }],
 		src: "https://i.imgur.com/jlFgGpe.jpeg",
-	},{ titel:"Titel",stats: { upvotes: 2,views:5,comments:1 },
+	},{ id:5, title:"Titel",stats: { upvotes: 2,upvoted: false,views:5,comments:1,clicked: false },comments:[{ name:"Ines",text:"I love cats" },{ name:"Felix",text:"I don't like cats" }],
 		src: "https://i.imgur.com/K6U0L3q.jpeg",
-	},{ titel:"Titel",stats: { upvotes: 2,views:5,comments:1 },
+	},{ id:6,title:"Titel",stats: { upvotes: 2,upvoted: false,views:5,comments:1,clicked: false },comments:[{ name:"Ines",text:"I love cats" },{ name:"Felix",text:"I don't like cats" }],
 		src: "https://i.imgur.com/rFBiyeR.jpeg",
-	},{ titel:"Langer Titel",stats: { upvotes: 2,views:5,comments:1 },
+	},{ id:7, title:"Langer Titel",stats: { upvotes: 2,upvoted: false,views:5,comments:1,clicked: false },comments:[{ name:"Ines",text:"I love cats" },{ name:"Felix",text:"I don't like cats" }],
 		src: "https://i.imgur.com/g81WFjQ.jpeg",
-	},{ titel:"Titel",stats: { upvotes: 2,views:5,comments:1 },
+	},{ id:8,title:"Titel",stats: { upvotes: 2,upvoted: false,views:5,comments:1,clicked: false },comments:[{ name:"Ines",text:"I love cats" },{ name:"Felix",text:"I don't like cats" }],
 		src: "https://i.imgur.com/0LINzxs.jpeg",
-	},{ titel:"Titel",stats: { upvotes: 2,views:5,comments:1 },
+	},{ id:9,title:"Titel",stats: { upvotes: 2,upvoted: false,views:5,comments:1,clicked: false },comments:[{ name:"Ines",text:"I love cats" },{ name:"Felix",text:"I don't like cats" }],
 		src: "https://i.imgur.com/xp4WKjC.jpeg",
 	}];
 
 </script>
 <div class="grid">
 	{#each images as image}
-		<Frame title={image.titel} image={image.src} upvotes={image.stats.upvotes} views={image.stats.views} comments={image.stats.comments} />
+		<Frame  title={image.title} image={image.src} bind:upvotes={image.stats.upvotes} bind:upvoted={image.stats.upvoted} bind:views={image.stats.views} comments={image.stats.comments} bind:clicked={image.stats.clicked} />
+		<Overlay bind:open={image.stats.clicked}>
+			<DetailView title={image.title} image={image.src} upvotes={image.stats.upvotes} bind:comments={image.comments} bind:views={image.stats.views} /></Overlay>
 	{/each}
 </div>
+
 
 <style>
 	.grid{
