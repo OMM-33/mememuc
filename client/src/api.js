cfef0f286012ef88d67d71176a132d3f036834b3
Muhlex <21311428+Muhlex@users.noreply.github.com>
2023-02-22 17:49:16 +0100

    Add very basic client auth framework-stuff

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,13 @@
-export const buildURL = url => new URL(url, import.meta.env.VITE_SERVER_URL);
+import { jwt as jwtStore } from "./auth";
+
+let jwt;
+jwtStore.subscribe(value => jwt = value);
+
+export const buildURL = (url, { useAuth = true } = {}) => {
+	const resultURL = new URL(url, import.meta.env.VITE_SERVER_URL);
+	if (useAuth && jwt) resultURL.searchParams.append("jwt", jwt);
+	return resultURL;
+};
 
 export const jsonHeaders = {
 	"Accept": "application/json",
