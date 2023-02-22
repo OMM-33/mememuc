6fbe93c07184776167efbf6ecf681c4ca8a196f5
Muhlex <21311428+Muhlex@users.noreply.github.com>
2023-02-22 20:28:45 +0100

    Fix tts being in incosistent languages across browsers

--- client/src/speech.js
+++ client/src/speech.js
@@ -1,4 +1,5 @@
-export const tts = text => {
+export const tts = (text, { lang = "en-US" } = {}) => {
 	const utterance = new SpeechSynthesisUtterance(text);
+	utterance.lang = lang;
 	window.speechSynthesis.speak(utterance);
 };
