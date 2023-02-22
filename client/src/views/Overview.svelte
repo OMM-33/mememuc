d43874dfa6ae1122d9974803d196088d8dc7f018
Muhlex <21311428+Muhlex@users.noreply.github.com>
2023-02-22 20:21:26 +0100

    Only show public memes in overview

--- client/src/views/Overview.svelte
+++ client/src/views/Overview.svelte
@@ -4,11 +4,19 @@
 	import Frame from "../lib/View/Frame.svelte";
 
 	updateMemes();
+
+	const memesArray = [];
+	$: (async () => {
+		const memeStores = [...$memes.values()];
+		memeStores.forEach((store, i) => store.subscribe(value => memesArray[i] = value));
+	})();
+
+	$: publicMemes = memesArray.filter(({ privacy }) => privacy === "public");
 </script>
 
 <h1>Overview </h1>
 <div class="grid">
-	{#each [...$memes.values()] as meme}
+	{#each publicMemes as meme}
 		<Frame {meme} />
 	{/each}
 </div>
